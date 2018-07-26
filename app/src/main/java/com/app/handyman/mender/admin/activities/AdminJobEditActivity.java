b533f12b9c47f43e5c27b9d2d382c7b1f3843c49
Ramesh Jangama <ramesh.btech@gmail.com>
2018-07-26 19:37:40 +0530

    test

--- app/src/main/java/com/app/handyman/mender/admin/activities/AdminJobEditActivity.java
+++ app/src/main/java/com/app/handyman/mender/admin/activities/AdminJobEditActivity.java
@@ -11,5 +11,6 @@ public class AdminJobEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_job_edit);
+        /// change test
     }
 }
